b4ff3deb94f731d120c69395140290cb72a44a59
sandra-mutai <sandramuttai@gmail.com>
2021-03-27 11:30:01 +0300

    second commit

---
+++ src/datatypes.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var name="sandra"
+    name="sandra"
+    println(name)
+
+    var age=19
+    println(age)
+
+    var citizenship="true"
+    println(citizenship)
+
+    var weight=60.5467328
+    println(weight)
+    var phoneNumber:String="0731405534"
+    println(phoneNumber)
+}
